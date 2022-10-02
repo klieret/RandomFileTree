1134fa5f82e94e6449c1287d01ef9842417aef9c
Don Heshanthaka <heshanthaka007@gmail.com>
2022-10-02 21:16:47 +0530

    Removed Shell=True from test_cli method

--- randomfiletree/test/test_cli.py
+++ randomfiletree/test/test_cli.py
@@ -11,7 +11,7 @@ from randomfiletree.cli import parser, cli
 class TestCli(unittest.TestCase):
     def test_cli(self):
         with tempfile.TemporaryDirectory() as dirname:
-            subprocess.run(["randomfiletree", dirname], shell=True)
+            subprocess.run(["randomfiletree", dirname])
 
     def test_cli_module(self):
         with tempfile.TemporaryDirectory() as dirname:
